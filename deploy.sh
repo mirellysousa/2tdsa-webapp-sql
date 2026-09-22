5e46978c084a10565658d22e760ceb037c66ef24
Mirelly Sousa <rm566299@fiap.com.br>
2026-09-22 10:07:47 -0300

    .

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Deploy da aplicação
-az webapp deploy --resource-group $RG_NAME --name $WEBAPP_NAME --scr-path ../transacoes-3/target/transacoes..jar --type jar
+az webapp deploy --resource-group $RG_NAME --name $WEBAPP_NAME --scr-path ../transacoes-3/target/transacoes.jar --type jar
